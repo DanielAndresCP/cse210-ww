801a412050c5f77f3f4bcb4977b2e8e475ee301c
Daniel Concha <danielconchaparra@gmail.com>
2024-03-25 20:10:18 -0300

    W05 LA - Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("purple", 5);
+        Rectangle rectangle = new Rectangle("red", 3, 10);
+        Circle circle = new Circle("blue", 4);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape item in shapes)
+        {
+            Console.WriteLine($"shape, Color: {item.GetColor()}, Area: {item.GetArea()}");
+        }
     }
 }
